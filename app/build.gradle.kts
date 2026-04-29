fdb7c75f40db932e07b2546f16c70a1a4cae9d20
Avishek Shrestha <abhishrestha987@gmail.com>
2026-04-29 13:06:07 +0545

    Gradle Setup and Initial Commit for gRPC

---
+++ app/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+
+    id("com.google.protobuf")
+}
+
+android {
+    namespace = "com.example.grpc_poc_shoutbox"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.grpc_poc_shoutbox"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Core Coroutines support
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+
+    // Android-specific support (for Dispatchers.Main etc.)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+
+    // gRPC dependencies
+    implementation("io.grpc:grpc-okhttp:1.63.0")// Transport for Android (uses OkHttp)
+    implementation("io.grpc:grpc-stub:1.63.0")// For calling RPC methods
+    implementation("io.grpc:grpc-protobuf-lite:1.63.0")// Protobuf support (lite = smaller + Android-optimized)
+    implementation("javax.annotation:javax.annotation-api:1.3.2")// Needed for generated code (like @Generated)
+}
+
+// gRPC code generation block
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+    plugins {
+        register("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.63.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                register("grpc") {
+                    option("lite")
+                }
+            }
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
